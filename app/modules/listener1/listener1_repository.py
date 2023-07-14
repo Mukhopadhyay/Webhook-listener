df19428e9d8a76ae4a6caf763c9a755d4a6c8614
Mukhopadhyay <praneshmukherjee7@gmail.com>
2023-07-14 18:23:05 +0530

    Adding base service & repo classes

---
+++ app/modules/listener1/listener1_repository.py
@@ -0,0 +1,6 @@
+from modules.base.repository import BaseRepository
+
+
+class Listener1Repository(BaseRepository):
+    def __init__(self) -> None:
+        super().__init__()
---
+++ app/modules/listener1/listener1_service.py
@@ -0,0 +1,6 @@
+from modules.base.service import BaseService
+
+
+class Listener1Service(BaseService):
+    def __init__(self) -> None:
+        super().__init__()
